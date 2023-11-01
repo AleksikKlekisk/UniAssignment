88fb30204fd01984bbe27b2525a905c231ead085
Aleksikツ <113190896+AleksikKlekisk@users.noreply.github.com>
2023-11-01 16:37:27 +0000

    Added InputManager

---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour {
+    static PlayerInputManager _instance;
+
+    public static PlayerInputManager Instance {
+        get {
+            return _instance;
+        }
+    }
+    PlayerControls PlayerControls;
+
+    public Vector2 movementInput;
+    private void Awake() {
+        if(_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        }
+        else {
+            _instance = this;
+        }
+        if(PlayerControls == null) {
+            PlayerControls = new PlayerControls();
+        }
+    }
+
+    private void Start() {
+        DontDestroyOnLoad(gameObject);
+    }
+    private void OnEnable() {
+        PlayerControls.Enable();
+    }
+
+    private void OnDisable() {
+        PlayerControls.Disable();
+    }
+    public Vector2 GetPlayerMovement() {
+        return PlayerControls.Player.Movement.ReadValue<Vector2>();
+    }
+
+    public bool HasPlayerJumped() {
+        return PlayerControls.Player.Jump.triggered;
+    }
+
+    private void Update() {
+        movementInput = GetPlayerMovement();
+        //remove this after testing.
+        if(HasPlayerJumped()) {
+            Debug.Log("Player has jumped");
+        }
+    }
+}
+
+
